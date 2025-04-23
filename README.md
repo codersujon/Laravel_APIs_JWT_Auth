4ed6e5612b225780adb65102f14db23c70cf9d38
MD. SUJAN AHMED <csesujon155@gmail.com>
2025-04-23 02:46:52 -0700

    Laravel Fresh Install, and configure

---
+++ README.md
@@ -0,0 +1,10 @@
+# 👨‍💼 Laravel REST APIs Development JWT (JSON Web Token)
+
+The default Users and Courses API Development with the help of JWT Authentication.
+
+---
+
+## 🌐 Base URL
+http://localhost:8000/api/
+
+---
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel JWT APIs</title>
+</head>
+<body>
+    <h1 align="center">Laravel JWT APIs Development</h1>
+</body>
+</html>
