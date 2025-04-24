9d977be2ec0e29f80028f7a6e6022a3954587107
MD. SUJAN AHMED <csesujon155@gmail.com>
2025-04-23 20:32:31 -0700

    User & Course Model, Migration, Controller and Configure Done!

---
+++ app/Http/Controllers/Api/CourseController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Course;
+
+class CourseController extends Controller
+{
+    /**
+     * Course Entrollment (POST, formdata)
+     */
+    public function courseEntroll(Request $request){
+
+    }
+
+    /**
+     * List Courses by User ID (GET)
+     */
+    public function listCourses(){
+
+    }
+
+    /**
+     * Delete User Course (DELETE)
+     */
+    public function deleteCourse($course_id){
+
+    }
+
+
+}
---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    /**
+     * User Register (POST, formdata)
+     */
+    public function register(Request $request){
+
+    }
+
+    /**
+     * User Login (POST, formdata)
+     */
+    public function login(Request $request){
+
+    }
+
+    /**
+     * User Profile (GET)
+     */
+    public function profile(){
+
+    }
+
+     /**
+     * User Logout (GET)
+     */
+    public function logout(){
+
+    }
+}
